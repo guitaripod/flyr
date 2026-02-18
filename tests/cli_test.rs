b6275d52c726ded4650011aa7254565c918af096
guitaripod <guitaripod@icloud.com>
2026-02-18 19:24:28 +0200

    Release v1.5.0

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -25,7 +25,7 @@ fn top_level_version() {
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("flyr 1.4.1"));
+        .stdout(predicate::str::contains("flyr 1.5.0"));
 }
 
 #[test]
